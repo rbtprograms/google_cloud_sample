585946357f26a94316c2f383b9c2cadcf8b5f33e
Robert Thompson <rbtprograms@gmail.com>
2019-09-12 06:33:55 -0700

    starting to http server and working through the first chapter of videos

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Welcome to my go server")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
